9f25598df24f6e7cdf9bfdb6d45392a3fa6833b0
TheMrDhiraj <themrdhiraj@gmail.com>
2020-06-24 10:46:45 +0545

    Menu active/inactive

--- resources/views/user/inc/menu.blade.php
+++ resources/views/user/inc/menu.blade.php
@@ -6,46 +6,22 @@
                 <img src="{{ asset('assets') }}/images/icons/logo-01.png" alt="LOGO">
             </a>
             <ul class="main-menu">
-                <li class="main-menu-active">
-                    <a href="/">Home</a>
-                </li>
-                <li class="mega-menu-item">
-                    <a href="/category">News</a>
-                    <div class="sub-mega-menu">
-                        <div class="nav flex-column nav-pills" role="tablist">
-                            <a class="nav-link active" data-toggle="pill" href="#news-0" role="tab">All</a>
-                            @if (count($category))
-                            @php
-                            $i = 1;
-                            @endphp
-                            @foreach ($category as $cat)
-                            <a class="nav-link" data-toggle="pill" href="#news-{{$i++}}" role="tab">{{$cat->type}}</a>
-                            @endforeach
-                            @endif
-                        </div>
-                    </div>
-                </li>
                 @if (count($category))
-                @foreach ($category as $cat)
-                @if ($cat == ($category[8]) ?? '')
-                @php
-                break ;
-                @endphp
-                @endif
-                <li>
-                    <a href="/category/{{$cat->id}}">{{$cat->type}}</a>
-                    @if (count($cat->subType))
+                @foreach ($category as $cats)
+                <li class="@if ($cat->p_id == $cats->id)
+                    main-menu-active
+                @endif">
+                    <a href="/category/{{$cats->id}}">{{$cats->type}}</a>
+                    @if (count($cats->subType))
                     <ul class="sub-menu">
-                        @foreach ($cat->subType as $sub)
-                        <li><a href="category-01.html">{{$sub->type}}</a></li>
+                        @foreach ($cats->subType as $sub)
+                        <li><a href="/category/{{$sub->id}}">{{$sub->type}}</a></li>
                         @endforeach
                     </ul>
                     @endif
                 </li>
                 @endforeach
                 @endif
-                
-                
             </ul>
         </nav>
     </div>
